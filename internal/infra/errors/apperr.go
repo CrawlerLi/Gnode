30b700bcc1a33b79f073e02456ecf4c7d6c45455
CrawlerLi <605069764@qq.com>
2026-05-17 21:07:02 +0800

    add errors moudle

---
+++ internal/infra/errors/apperr.go
@@ -0,0 +1,33 @@
+package errors
+
+type ErrorCode int
+
+const (
+	//业务错误码
+	ErrInvaidTX = iota
+	ErrInsufficientFunds
+	ErrNotFundTX
+)
+
+var ErrorCodeStrings = map[ErrorCode]string{
+	ErrInvaidTX:          "ErrInvaildTX",
+	ErrInsufficientFunds: "ErrInsufficientFunds",
+	ErrNotFundTX:         "ErrNotFundTX",
+}
+
+type Error struct {
+	ErrorCode  ErrorCode
+	Descripton string
+	Err        error
+}
+
+func (e Error) Error() string {
+	if e.Err != nil {
+		return e.Descripton + ": " + e.Err.Error()
+	}
+	return e.Descripton
+}
+
+func makeError(c ErrorCode, desc string, err error) Error {
+	return Error{ErrorCode: c, Descripton: desc, Err: err}
+}
